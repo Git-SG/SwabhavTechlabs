0a529f9703dfdac33f3a1ad4c320f5badfd6c8d0
Git-SG <100405739+Git-SG@users.noreply.github.com>
2024-07-24 23:34:19 +0530

    Add files via upload

---
+++ RefactoredPIG/RefactoredPIGGame.java
@@ -0,0 +1,11 @@
+package H9_ObjectCalesthenics_test;
+
+import H9_ObjectCalesthenics_model.Game;
+
+public class RefactoredPIGGame {
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.playGame();
+	}
+
+}
